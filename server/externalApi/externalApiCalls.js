c64f4d829f4c14a1f7517b0e9f03493093557d5e
ZyAjahT <zyajah@gmail.com>
2025-04-26 01:47:37 -0400

    Added current and alerts external api calls

--- server/externalApi/externalApiCalls.js
+++ server/externalApi/externalApiCalls.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
- const apiKey = 'YouApiKey'
+const apiKey = '148c3ceb8a084fc0bc903924252404'; 
  
  module.exports = {
      forecastApi: async (location, days) => {
@@ -12,6 +12,30 @@ const axios = require('axios');
                  return{ error: "Unable to fetch forecast information." }
              })
      },
+
+     currentApi: async (location) => {
+        return await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`)
+            .then(function (response) {
+               return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                return{ error: "Unable to fetch current information." }
+            })
+    },
+
+    AlertsApi: async (location) => {
+        return await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`)
+            .then(function (response) {
+               return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                return{ error: "Unable to fetch alerts information." }
+            })
+    },
+
+
      // add alert API call hint: change forecast.json to match api wanted 
      // add current API
      // documentation https://www.weatherapi.com/docs/#intro-request
